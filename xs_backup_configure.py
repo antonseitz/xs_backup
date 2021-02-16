2f91cf5fcf15991163da9967a045fe787b93f088
Anton Seitz <aseitz@puckit.de>
2021-02-16 09:28:05 +0100

    Crontab: daily mo-fr, weekly sa

--- xs_backup_configure.py
+++ xs_backup_configure.py
@@ -22,8 +22,8 @@ if ask("Should we add an cronjob ? ")=="":
 	
     crontab=open("/var/spool/cron/root", "a")
     
-    crontab.write( time[0]+ " " + time[1] +" * * * " + path + "/VmBackup/VmBackup.py " + path + "/pwd.file " + path + "/daily.cfg >> " + path + "/logs/" + hostname + ".log 2>&1\n")
-    crontab.write( time[0]+ " " + time[1] +" * * * " + path + "/VmBackup/VmBackup.py " + path + "/pwd.file " + path + "/weekly.cfg >> " + path + "/logs/" + hostname + ".log 2>&1\n")
+    crontab.write( time[0]+ " " + time[1] +" 1-5 * * " + path + "/VmBackup/VmBackup.py " + path + "/pwd.file " + path + "/daily.cfg >> " + path + "/logs/" + hostname + ".log 2>&1\n")
+    crontab.write( time[0]+ " " + time[1] +" 6 * * " + path + "/VmBackup/VmBackup.py " + path + "/pwd.file " + path + "/weekly.cfg >> " + path + "/logs/" + hostname + ".log 2>&1\n")
     crontab.close()
 else:
     print("Skipped!")
